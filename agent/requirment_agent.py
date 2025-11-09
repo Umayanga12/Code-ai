fc2a3d04dafcb261f31a296d6aded93238bb5bbe
Umayanga <umayangaathapaththu05@gmail.com>
2025-11-09 15:46:36 +0530

    debuggign

--- agent/requirment_agent.py
+++ agent/requirment_agent.py
@@ -1,7 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+sys.path.insert(1, os.path.join(".", "core"))
+sys.path.insert(1, os.path.join(".", "prompt"))
+sys.path.insert(1, os.path.join(".", "responce_model"))
+
 from langchain.agents import create_agent
 from langchain.agents.structured_output import ToolStrategy
 
-from llm import llm
+from core.llm import llm
 from prompt.req_gather_prompt import PROJECT_REQUIREMENT_GATHERING
 from responce_model.requrementModel import CompleteRequirement
 
