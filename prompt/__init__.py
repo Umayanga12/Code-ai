b8c6cf3e97ff89c0f38eae24b95ec4abbcc75b7d
Umayanga <umayangaathapaththu05@gmail.com>
2025-11-09 15:47:30 +0530

    refector

--- prompt/__init__.py
+++
@@ -1,5 +0,0 @@
-from prompt.req_gather_prompt import PROJECT_REQUIREMENT_GATHERING
-
-__all__ = [
-    "PROJECT_REQUIREMENT_GATHERING",
-]
--- responce_model/requrementModel.py
+++ responce_model/requrementModel.py
@@ -1,5 +1,4 @@
 from typing import List, Optional
-from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field
 
