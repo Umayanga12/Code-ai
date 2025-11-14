c982ab58c1d7f7a02a4cd99324325f20f7276ba1
Umayanga <umayangaathapaththu05@gmail.com>
2025-11-14 15:06:51 +0530

    db-commit

--- config/config.py
+++ config/config.py
@@ -22,3 +22,21 @@ settings = Settings(
 # Fail fast if essential keys are missing
 if not settings.OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY environment variable not set.")
+
+
+class DBSettings(BaseModel):
+    DB_USER_NAME: str = ""
+    DB_PASSWORD : str = ""
+    DB_HOST : str =  ""
+    DB_NAME : str = ""
+
+dbSettings = DBSettings(
+    DB_HOST= os.getenv("DB_HOST")  or "",
+    DB_PASSWORD = os.getenv("DB_PASSWORD") or "",
+    DB_USER_NAME= os.getenv("DB_USERNAME") or "",
+    DB_NAME= os.getenv("DB_NAME") or "",
+)
+#print(dbSettings)
+
+if dbSettings.DB_HOST == "" or dbSettings.DB_PASSWORD == "" or dbSettings.DB_USER_NAME == "" or dbSettings.DB_NAME == "":
+    raise ValueError("Database configurations not set")
